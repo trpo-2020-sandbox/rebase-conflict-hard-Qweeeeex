2f0f694fd148e13ba32db8897d8ba54845af70d2
srwxrwxrwx <srwxrwxrwx@users.noreply.github.com>
2017-03-04 17:43:05 +0700

    Extract printing array to function

--- main.c
+++ main.c
@@ -16,17 +16,33 @@ int max_element(int* arr, int size)
     return max;
 }
 
-int main()
+void print_array(int* arr, int size)
 {
+<<<<<<< HEAD
     int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
     const int max = max_element(arr, N_ELEMENTS(arr));
+||||||| constructed merge base
+    int arr[] = {3, 1, 4, 1, 5};
+
+    const int max = max_element(arr, N_ELEMENTS(arr));
+
+=======
+>>>>>>> Extract printing array to function
     printf("Array: ");
     int i;
-    for (i = 0; i < N_ELEMENTS(arr); ++i) {
+    for (i = 0; i < size; ++i) {
         printf("%d ", arr[i]);
     }
     printf("\n");
+}
 
+int main()
+{
+    int arr[] = {3, 1, 4, 1, 5};
+
+    const int max = max_element(arr, N_ELEMENTS(arr));
+
+    print_array(arr, N_ELEMENTS(arr));
     printf("Max element: %d\n", max);
 
     return 0;
